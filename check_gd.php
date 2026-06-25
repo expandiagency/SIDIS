7ce000d3db5846b560f6a561bbe6ff830a4b0c46
Vladyslav <vladyslav@MacBook-Air-Vladyslav.local>
2026-06-25 22:33:42 +0200

    Remove temporary GD check script

--- check_gd.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "GD: " . (extension_loaded('gd') ? 'yes' : 'no') . "\n";
-echo "Imagick: " . (extension_loaded('imagick') ? 'yes' : 'no') . "\n";
-if (extension_loaded('gd')) {
-    $info = gd_info();
-    echo "WebP support: " . (!empty($info['WebP Support']) ? 'yes' : 'no') . "\n";
-}
-echo "PHP version: " . PHP_VERSION . "\n";
